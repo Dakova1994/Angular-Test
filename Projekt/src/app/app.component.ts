27d3178d4ec63548edbc3bd01d70649e7cb5911c
Dakova <cejba.patryk@gmail.com>
2018-02-19 22:37:43 +0100

    Add ngOnInit - > cardsServices && getCards

--- Projekt/src/app/app.component.ts
+++ Projekt/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardsService} from "./app.service"
+import { element } from 'protractor';
+import { IDeck } from "./deck.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  twoCards:IDeck;
+  deck:IDeck;
+  constructor (private cardsService: CardsService){}
+  ngOnInit(){
+    this.cardsService.getShuffledDeck().subscribe( results=> {
+        this.deck=results;
+        console.log("moje rezultaty:", this.deck)
+    this.cardsService.getCards(this.deck.deck_id).subscribe(results =>{
+      this.twoCards=results;
+      console.log("dwie karty:", this.twoCards);
+    });
+  }
+
 }
--- Projekt/src/app/app.module.ts
+++ Projekt/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-
+import { CardsService } from './app.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule
   ],
-  providers: [],
+  providers: [CardsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
