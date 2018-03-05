4c9e0c67aacbcea9f2a3bdd8b83ff152a7dac527
Dakova <cejba.patryk@gmail.com>
2018-03-05 19:49:30 +0100

    Created checkbox for drawn cards from new deck

--- Projekt/src/app/app.component.ts
+++ Projekt/src/app/app.component.ts
@@ -12,7 +12,10 @@ export class AppComponent implements OnInit {
     public currentPile: IPile;
     public currentDeck: IDeck;
     public drawnCards = [];
+    public myCards = [];
     public numberOfCards = 0;
+    public x: number;
+    public isNewDeckChosen: boolean=false;
     constructor(private cardsService: CardsService) {}
     ngOnInit() {
         this.cardsService.getShuffledDeck().subscribe( shuffledDeck  => {
@@ -20,11 +23,31 @@ export class AppComponent implements OnInit {
         });
     }
     drawCards() {
-        this.cardsService.getCards(this.currentDeck.deck_id, this.numberOfCards).subscribe( pile  => {
-            this.currentPile = pile ;
-            this.currentPile.cards.map(card => {
+        if(this.isNewDeckChosen){
+            this.drawnCards=[];
+            this.cardsService.getShuffledDeck().subscribe( shuffledDeck  => {
+                this.deck = shuffledDeck ;
+            });
+        }
+        this.cardsService.getCards(this.deck.deck_id, this.numberOfCards).subscribe( pile  => {
+            this.pile = pile ;
+            this.pile.cards.map(card => {
                 this.drawnCards.push(card);
+                console.log(this.drawCards);
             })
         });
+    
     }
+    isNextDeck(){
+        if(this.isNewDeckChosen){
+            this.isNewDeckChosen=false;
+        }
+        else{
+            this.isNewDeckChosen=true;
+        }
+    }
+    remainingCard(){
+        return 52-(this.drawnCards.length);
+    }
+  
 }
