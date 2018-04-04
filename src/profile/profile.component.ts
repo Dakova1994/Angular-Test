cac708888df480de2b1b04d0572d36b38d39151f
Dakova <cejba.patryk@gmail.com>
2018-04-04 21:56:58 +0200

    added profile

---
+++ src/profile/profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css'],
+})
+export class ProfileComponent{
+
+}
